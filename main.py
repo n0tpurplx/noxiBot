ee134f8072c440ca4b7d804c8f974ae68d54df54
n0tpurplx <practicalcharacter@gmail.com>
2026-08-26 18:02:34 +0200

    Modify bot intents and add presence update
    Updated bot intents to enable message content and added presence change on ready event.

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="_", intents=discord.Intents.(messages=True))
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="_", intents=intents)
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
+
+    await bot.change_presence(
+        status=discord.Status.dnd,
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name=f"{len(bot.guilds)} servers"
+        )
+    )
+
     print(f"Logged in as {bot.user}")
 
 @bot.tree.command(name="ping")
